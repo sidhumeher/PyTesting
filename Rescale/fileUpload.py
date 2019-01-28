737cc5b3343847ef383989934e64c713eed27bb0
Sid <sidhumeher@yahoo.co.in>
2019-01-28 13:43:10 -0800

    Rescale file upload

---
+++ Rescale/fileUpload.py
@@ -0,0 +1,57 @@
+'''
+Created on Jan 21, 2019
+
+@author: siddardha.teegela
+'''
+
+import time
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ChromeOptions, Chrome
+import autoit
+
+def rescale_upload():
+    user = "xxxxxx"
+    pwd = "xxxxxx"
+    
+    # For Chrome
+    opts = ChromeOptions()
+    opts.add_experimental_option("detach", True)
+    
+    driver = webdriver.Chrome("<chromedriver-path>",options=opts)
+    driver.get("https://platform.rescale.com/")
+    
+    #Find login
+    element = driver.find_element_by_id("id_username")
+    element.send_keys(user)
+    
+    element = driver.find_element_by_id("id_password")
+    element.send_keys(pwd)
+    
+    element.send_keys(Keys.RETURN)
+    
+    time.sleep(3)
+    #file_click = driver.find_element_by_id("menuFiles")
+    file_click = driver.find_element_by_xpath("//*[@id='menuFiles']/a")
+    file_click.click()
+    
+    time.sleep(10)
+    
+    try:
+        upload_file = driver.find_element_by_xpath("//*[@id='filesPageDropZone']")
+        upload_file.click()
+        time.sleep(5)
+        autoit.win_active("Open")
+        autoit.control_send("Open", "Edit1",r"<file-path>")
+        autoit.control_send("Open","Edit1","{ENTER}")
+    except Exception as e:
+        print('Could not upload file')
+    
+    time.sleep(50)
+    
+    #driver.close()
+
+if __name__ == '__main__':
+    #pass
+    rescale_upload()
---
+++ Rescale/fileUploadWithAPI.py
@@ -0,0 +1,38 @@
+'''
+Created on Jan 28, 2019
+
+@author: siddardha.teegela
+'''
+import requests
+
+def file_uplpad_with_api():
+    api_url = 'https://platform.rescale.com/api/v2/files/contents/'
+    key = 'api-key'
+    try:
+        with open('<file-path>','rb') as upload_file:
+            response = requests.post(api_url,headers = {'Authorization': 'Token '+key},files={'file': upload_file})
+            print(response.status_code)
+            print(response.text)
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+    #pass
+    file_uplpad_with_api()
+    
+
+'''
+Response:
+201
+{"pathParts":{"path":"user/user_EUuNk/testdoc_api-5cbab800-dd06-4870-87b5-d1daa4d129b3.txt",
+"container":"prod-rescale-platform"},"typeId":1,"name":"testdoc_api.txt",
+"dateUploaded":"2019-01-28T21:34:04.447607Z","relativePath":"testdoc_api.txt",
+"storage":{"storageType":"S3Storage","id":"pCTMk",
+"connectionSettings":{"region":"us-east-1"},"encryptionType":"default"},
+"decryptedSize":151,"downloadUrl":"https://platform.rescale.com/api/v2/files/PCJNzf/contents/",
+"sharedWith":[],"encodedEncryptionKey":"MggLWPwsdYjn8aIlYlwL8kndsvu66i2qAbMAqXj/bgg=",
+"owner":"sidhumeher@yahoo.co.in",
+"path":"user/user_EUuNk/testdoc_api-5cbab800-dd06-4870-87b5-d1daa4d129b3.txt",
+"isUploaded":true,"viewInBrowser":true,"id":"PCJNzf","isDeleted":false,
+"md5":"20ae7043ab52c870a79ab94e59a82309"}
+'''
